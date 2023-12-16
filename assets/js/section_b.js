ec6056e3cfa22c98dd8614aea37849339634fc81
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-12-16 16:13:29 +0000

    refactor: add proper comments to the EmailJS integration

--- assets/js/section_b.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("bnext").onclick = function () {
-  let lists = document.querySelectorAll(".bitem");
-  document.getElementById("bslide").appendChild(lists[0]);
-};
-document.getElementById("bprev").onclick = function () {
-  let lists = document.querySelectorAll(".bitem");
-  document.getElementById("bslide").prepend(lists[lists.length - 1]);
-};
