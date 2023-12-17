2815cdfaf587f424376ba393865034eb7d77c348
Volodymyr Popov <smartappsvp@gmail.com>
2023-12-17 13:16:52 +0000

    add build markup function fix css

--- assets/js/section_b.js
+++ assets/js/section_b.js
@@ -1,8 +1,50 @@
-document.getElementById("bnext").onclick = function () {
+const galleryContainer = document.getElementById("bslide");
+
+document.querySelector("#bnext").onclick = function () {
   let lists = document.querySelectorAll(".bitem");
-  document.getElementById("bslide").appendChild(lists[0]);
+  galleryContainer.appendChild(lists[0]);
 };
-document.getElementById("bprev").onclick = function () {
+
+document.querySelector("#bprev").onclick = function () {
   let lists = document.querySelectorAll(".bitem");
-  document.getElementById("bslide").prepend(lists[lists.length - 1]);
+  galleryContainer.prepend(lists[lists.length - 1]);
 };
+
+// Fetch JSON data
+async function bFetchJson() {
+  try {
+    const response = await fetch("assets/data/recipies_food.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching JSON data: ", error);
+  }
+}
+
+function createImgList(arr) {
+  return arr
+    .map(
+      ({ id, name, country, image_url }) =>
+        `
+                    <div
+              class="bitem"
+              style="background-image: url('${image_url}')"
+            >
+              <div class="bholder">
+                <h3 class="bcard-text">${country}</h3>
+              </div>
+              <div class="bcontent">
+                <div class="bname">${country}</div>
+                <div class="bdes">${name}</div>
+                 <a class="bToCalendar" data-id="${id}" href="#section-h">SEE MORE</a>
+              </div>
+            </div>
+      `
+    )
+    .join("");
+}
+
+bFetchJson().then((data) => {
+  galleryContainer.insertAdjacentHTML("afterbegin", createImgList(data));
+  galleryContainer.innerHTML(createImgList(data));
+});
